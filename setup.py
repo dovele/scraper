9402ba4ee650b2e8decb53ec928e06c2377377fe
Dovilė E <75432544+dovele@users.noreply.github.com>
2021-08-08 23:50:47 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+#from distutils.core import setup
+
+setup(
+  name='scraper',
+  version='1.1.0',
+  author='dovele',
+  url='https://github.com/dovele/scraper')
