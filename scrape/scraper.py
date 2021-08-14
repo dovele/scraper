e809bbd38879e89813408d5fa49f3d012a71bae8
Dovilė E <75432544+dovele@users.noreply.github.com>
2021-08-14 21:26:36 +0300

    Update scraper.py

--- scrape/scraper.py
+++ scrape/scraper.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from csv import reader
-import re
 
 headers = {'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
